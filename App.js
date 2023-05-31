d0be29a9b3d2b72bd87e582109e40eba349bb7f7
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-05-31 22:01:03 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Form Example</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
